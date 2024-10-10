04dad1c95ef84750184acdb29eb64df4877449af
romain65000 <148979065+romain65000@users.noreply.github.com>
2024-10-10 09:45:19 +0200

    Update data.sql

---
+++ fichiers de projets/config/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Utilisateur (nom, prenoms, email, mot_de_passe, date_naissance, numero_securite_sociale, telephone, adresse, code_postal)
+VALUES ('Dupont', 'Jean', 'jean.dupont@example.com', 'hashed_password', '1980-05-15', '123-45-6789', '0123456789', '123 Rue Principale', '75001');
